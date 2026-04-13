ddbb979969e4700476eed8e49509d4e32c77a646
3diso <edyspaulet@gmail.com>
2026-04-13 16:38:58 +0000

    Added multi threading

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 use std::{
     fs,
     io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream}, str::FromStr,
+    net::{TcpListener, TcpStream},
+    str::FromStr,
+    thread,
+    time::Duration,
 };
 
 fn main() {
-    let listener = TcpListener::bind("2a0c:b381:803:4500:e7a0:1ed4:9462:7d54:443").unwrap();
+    let listener = TcpListener::bind("[::]:31233").unwrap();
+
+    //let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        let res = handle_connection(stream);
-        println!("{res}");
+        thread::spawn(|| {
+            println!("{}",handle_connection(stream));
+        });
     }
 }
-fn handle_connection(mut stream: TcpStream) -> String{
+
+fn handle_connection(mut stream: TcpStream) -> String {
     println!("Appeared");
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next();
 
-    let content; 
+    let content;
     match request_line {
         Some(contents) => content = contents,
         None => return String::from_str("Fail").unwrap(),
@@ -31,17 +38,19 @@ fn handle_connection(mut stream: TcpStream) -> String{
         Err(err) => return String::from_str("Fail").unwrap(),
     };
 
-    let (status_line, filename) = if result == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &result[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
     String::from_str("Success").unwrap()
